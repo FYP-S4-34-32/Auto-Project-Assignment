16ea1e63971abace945dfab71b231cbd4aa6cacb
ehjujube <yinli025@gmail.com>
2022-11-11 13:18:15 +0800

    added password toggle button

--- Code/frontend/src/pages/Login.js
+++ Code/frontend/src/pages/Login.js
@@ -10,6 +10,7 @@ const Login = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const { login, error, isLoading } = useLogin() // from useLogin.js in the hooks folder
+    const [passwordShown, setShowPassword] = useState(false); // show password
 
     const handleSubmit = async (e) => {
         // prevent refresh upon submit
@@ -19,6 +20,12 @@ const Login = () => {
         await login(email, password)
     }
 
+    // show Password toggle 
+    const showPassword = () => {
+        // When the handler is invoked
+        // inverse the boolean state of passwordShown
+        setShowPassword(!passwordShown);
+    };
     // return a template - login form
     return (
         <form className="login" onSubmit={handleSubmit}>
@@ -35,6 +42,7 @@ const Login = () => {
                 onChange={(e) => {setPassword(e.target.value)}} // set password to the value of the target input field
                 value={password} // reflect change in password state
             />
+            <button className="showPwBtn" onClick={showPassword}>Show Password</button>
 
             <button disabled={ isLoading }>Login</button> {/*prevent button from being clicked while page is loading*/}
             {error && <div className="error">{ error }</div>}
