b42b571cf315eeabaea4966e011f07bf42dc7f82
mattyjy <104262448+mattyjy@users.noreply.github.com>
2022-11-18 05:31:33 +0800

    added projectdetails.js

--- Code/frontend/src/pages/Signup.js
+++ Code/frontend/src/pages/Signup.js
@@ -9,6 +9,7 @@ import { useSignup } from '../hooks/useSignup'
 const Signup = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [role, setRole] = useState('')
     const { signup, isLoading, error } = useSignup() // from useSignup.js in the hooks folder
 
     const handleSubmit = async (e) => {
@@ -16,13 +17,13 @@ const Signup = () => {
         e.preventDefault()
 
         // invoke signup function from useSignup.js
-        await signup(email, password) 
+        await signup(email, password, role) 
     }
 
     // return a template - signup form
     return (
         <form className="signup" onSubmit={handleSubmit}>
-            <h3>Sign Up</h3>
+            <h3>Account Creation</h3>
             <label>Email:</label>
             <input
                 type="email"
@@ -35,7 +36,14 @@ const Signup = () => {
                 onChange={(e) => {setPassword(e.target.value)}} // set password to the value of the target input field
                 value={password} // reflect change in password state
             />
-
+            <label>Role:</label>
+            <select value={role} onChange={(e) => {setRole(e.target.value)}}>
+                <option value="Employee">Employee</option>
+                <option value="Admin">Admin</option>
+            </select>
+            <br></br>
+            <br></br>
+                
             <button disabled={ isLoading }>Sign up</button> {/*prevent button from being clicked while page is loading*/}
             {error && <div className="error">{ error }</div>}
         </form>
